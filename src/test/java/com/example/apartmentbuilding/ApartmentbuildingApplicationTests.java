439a869bf04f3a34e98cfc3e7412818b1b4ae4df
Ritik Gupta <ritikgupta@Ritiks-MacBook-Pro.local>
2025-03-03 17:48:42 +0530

    adding test class

--- src/test/java/com/example/apartmentbuilding/ApartmentbuildingApplicationTests.java
+++ src/test/java/com/example/apartmentbuilding/ApartmentbuildingApplicationTests.java
@@ -9,5 +9,45 @@ class ApartmentbuildingApplicationTests {
 	@Test
 	void contextLoads() {
 	}
+	
+	public static void main(String[] args) {
+        Building building = new Building(25.0);
+
+        building.addApartment(new Apartment("101", "John Doe"));
+        building.addApartment(new Apartment("102", "Jane Doe"));
+        building.addCommonRoom(new CommonRoom("Gym1", "Gym"));
+        building.addCommonRoom(new CommonRoom("Library1", "Library"));
+
+        building.recalculateHeatingCooling();
+
+        // Display room status
+        System.out.println("Building Status:");
+        for (Apartment apartment : building.getApartments()) {
+            System.out.println(apartment.getId() + " - Heating: " + apartment.isHeatingEnabled() +
+                    ", Cooling: " + apartment.isCoolingEnabled());
+        }
+        for (CommonRoom commonRoom : building.getCommonRooms()) {
+            System.out.println(commonRoom.getId() + " - Heating: " + commonRoom.isHeatingEnabled() +
+                    ", Cooling: " + commonRoom.isCoolingEnabled());
+        }
+
+        // Allow user to input new temperature
+        Scanner scanner = new Scanner(System.in);
+        System.out.print("Enter new requested temperature: ");
+        double newTemperature = scanner.nextDouble();
+        building.updateRequestedTemperature(newTemperature);
+
+        // Display updated status
+        System.out.println("Updated Building Status:");
+        for (Apartment apartment : building.getApartments()) {
+            System.out.println(apartment.getId() + " - Heating: " + apartment.isHeatingEnabled() +
+                    ", Cooling: " + apartment.isCoolingEnabled());
+        }
+        for (CommonRoom commonRoom : building.getCommonRooms()) {
+            System.out.println(commonRoom.getId() + " - Heating: " + commonRoom.isHeatingEnabled() +
+                    ", Cooling: " + commonRoom.isCoolingEnabled());
+        }
+    }
+
 
 }
